421027a5d707af6e8fb659beae9c9da23d3dc350
aminRmc <ramcilovicamin@gmail.com>
2024-01-19 09:31:52 +0100

    add final

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ let counts = setInterval(updated);
 let i = 0
 function updated() {
 
-    count[0].innerHTML = ++i;
+    count[0].innerHTML = i+=100;
     
-    if (i === 1000) {
+    if (i === 10000) {
         clearInterval(counts);
     }}
 
@@ -14,7 +14,7 @@ let counts2 = setInterval(updated2);
 let i2 = 0
 function updated2() {
 
-    count[1].innerHTML = ++i2;
+    count[1].innerHTML = i2 += 20;
     
     if (i2 === 8000) {
         clearInterval(counts2);
@@ -26,7 +26,7 @@ let counts3 = setInterval(updated3);
 let i3 = 0
 function updated3() {
 
-    count[2].innerHTML = ++i3;
+    count[2].innerHTML = i3+=20;
     
     if (i3 === 5000) {
         clearInterval(counts3);
